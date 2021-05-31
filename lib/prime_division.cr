8007c7567be01be1b06548347fa0b2a688f56a97
kthatoto <kthatoto@gmail.com>
2021-06-01 05:04:55 +0900

    Edit lib/prime_division.cr

--- lib/prime_division.cr
+++ lib/prime_division.cr
@@ -2,15 +2,16 @@ def prime_division(n : UInt64) : Hash(UInt64, UInt64)
   result = Hash(UInt64, UInt64).new
 
   m = 2_u64
-  while n > 1
+  i = n.dup
+  while i > 1 && m <= n**(1 / 2)
     c = 0_u64
-    while n % m == 0
-      n //= m
+    while i % m == 0
+      i //= m
       c += 1
     end
     result[m] = c if c > 0
     m += m == 2 ? 1 : 2
   end
 
-  result
+  result.size > 0 ? result : { n => 1_u64 }
 end
