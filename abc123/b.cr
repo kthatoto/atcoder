8e08c7e6d9bf306e896b454d71e2fa1029d32cdc
kthatoto <kthatoto@gmail.com>
2021-06-13 07:45:44 +0900

    Edit abc123/b.cr

--- abc123/b.cr
+++ abc123/b.cr
@@ -4,5 +4,5 @@ c = read_line.to_i
 d = read_line.to_i
 e = read_line.to_i
 
-dishes = [a, b, c, d, e].sort_by { |dish| -dish % 10 }
-puts dishes.pop + dishes.sum { |dish| dish + (10 - dish % 10) }
+dishes = [a, b, c, d, e].sort_by { |dish| ((dish - 1) % 10) }
+puts dishes[0] + dishes[1..].sum { |dish| dish + (dish % 10 == 0 ? 0 : (10 - dish % 10)) }
